3a31c5387efb04537c2440ad2693932ba1d7e989
Sandyvaranasi <tripathisn5363@gmail.com>
2022-12-08 22:59:27 +0530

    made some changes

--- src/routes/route.js
+++ src/routes/route.js
@@ -4,5 +4,9 @@ const router = express.Router();
 router.get('/test-me', function (req, res) {
     res.send('My first ever api!')
 });
+router.get('/test-you', function (req, res) {
+    console.log("I did it myself!")
+    res.send('Changes made by me!')
+});
 
 module.exports = router;
